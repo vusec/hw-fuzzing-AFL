636c7fcadb22083a45360c2b6e1b89d12863b899
Raphael Isemann <teemperor@gmail.com>
2025-01-29 18:24:25 +0100

    [hw-fuzzing] Add tests

---
+++ hw-fuzz-tests/test.cpp
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <tainting.h>
+
+const size_t size = 20;
+
+struct Step {
+  char storage[size];
+  Step(char x) {
+    memset(storage, x, size);
+  }
+
+  char getCharOrNull(size_t index) {
+    index -= '0';
+    if (index < size)
+      return storage[index];
+    return '0';
+  }
+
+  void set(size_t i, char x) {
+    if (i < size)
+      storage[i] = x;
+  }
+};
+
+__attribute__((noinline))
+void target_function(char *buf, size_t len) {
+  if (len < 6)
+    return;
+
+  Step step1('1');
+
+  Tainting::taintPtr((void*)(step1.storage + 4), 1);
+
+  Step step2('2');
+  step2.set(5, step1.getCharOrNull(*(++buf)));
+
+  Step step3('3');
+  step3.set(7, step2.getCharOrNull(*(++buf)));
+
+  Step step4('4');
+  step4.set(11, step3.getCharOrNull(*(++buf)));
+
+  if (step1.storage[0] == 'x') {
+    printf("got an x first\n");
+  }
+
+  if (step1.storage[0] == 'x') {
+    printf("second one is %d\n", (step1.storage[1] == 'y' ? 1 : 2));
+  }
+
+  Step step5('5');
+  step5.set(3, step4.getCharOrNull(*(++buf)));
+
+  if (Tainting::check(step5.storage + 3, 1)) {
+    fprintf(stderr, "Got magic input\n");
+    abort();
+  }
+
+  step4.storage[size - 1] = 0;
+  printf("%s\n", step4.storage);
+}
+
+
+#ifndef persistent
+__AFL_FUZZ_INIT();
+
+int main(int argc, char **argv) {
+
+#ifdef __AFL_HAVE_MANUAL_CONTROL
+  __AFL_INIT();
+#endif
+
+  unsigned char *buf = __AFL_FUZZ_TESTCASE_BUF;
+
+  while (__AFL_LOOP(10000)) {
+
+    int len = __AFL_FUZZ_TESTCASE_LEN;
+
+    if (len < 2) continue;
+
+    target_function((char *)(buf), len);
+
+  }
+
+  return 0;
+
+}
+#else
+
+int main(int argc, char **argv) {
+  FILE *f = fopen(argv[1], "rb");
+  fseek(f, 0, SEEK_END);
+  long fsize = ftell(f);
+  fseek(f, 0, SEEK_SET);
+
+  char *string = (char *)malloc(fsize + 1);
+  fread(string, fsize, 1, f);
+  fclose(f);
+
+  string[fsize] = 0;
+  target_function((char *)(string), fsize);
+}
+
+#endif
